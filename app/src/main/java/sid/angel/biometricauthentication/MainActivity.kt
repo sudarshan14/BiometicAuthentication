64759ad9b9af90f0b8f35dcb180c63ed8ac80e8d
sudarshan bhatt <sudarshan.bhatt@tcs.com>
2020-06-01 02:52:21 +0530

    Update gradle dependencies | code updated

--- app/src/main/java/sid/angel/biometricauthentication/MainActivity.kt
+++ app/src/main/java/sid/angel/biometricauthentication/MainActivity.kt
@@ -6,12 +6,13 @@ import android.util.Log
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
-    private lateinit var bioMetricManager: androidx.biometric.BiometricManager
-    private lateinit var executor: Executor
+    private lateinit var bioMetricManager: BiometricManager
+
     private lateinit var biometricPrompt: BiometricPrompt
     private lateinit var promptInfo: BiometricPrompt.PromptInfo
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,55 +21,83 @@ class MainActivity : AppCompatActivity() {
 
         bioMetricManager = BiometricManager.from(this)
 
-        checkBiometricStatus(bioMetricManager)
-        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = createBioMetricPrompt()
 
-        biometricPrompt =
-            BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+        promptInfo = createPromptInfo()
 
-                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                    super.onAuthenticationError(errorCode, errString)
-                }
+     //   biometricPrompt.authenticate(promptInfo)
 
-                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                    super.onAuthenticationSucceeded(result)
-                }
+        if (bioMetricManager
+                .canAuthenticate() == BiometricManager.BIOMETRIC_SUCCESS
+        ) {
+            biometricPrompt.authenticate(promptInfo)
+        } else {
+            checkBiometricStatus(bioMetricManager)
+        }
 
-                override fun onAuthenticationFailed() {
-                    super.onAuthenticationFailed()
-                }
-            })
+    }
 
-        promptInfo = BiometricPrompt.PromptInfo.Builder()
-            .setTitle("Hello")
-            .setDescription("There you are")
-            .setNegativeButtonText("CANCEL")
+    private fun createBioMetricPrompt(): BiometricPrompt {
+
+        val executor = ContextCompat.getMainExecutor(this)
+        val callback = object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Log.d(TAG, "{$errorCode = $errString}")
+
+                type.text = "{$errorCode = $errString}"
+
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Log.d(TAG, "Authentication failed for an unknown reason")
+                type.text = "Authentication failed for an unknown reason."
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+
+                Log.d(TAG, "Authentication was successful")
+                // Proceed with viewing the private encrypted message.
+                type.text = "Authentication was successful."
+            }
+
+        }
+
+        return BiometricPrompt(this, executor, callback)
+    }
+
+
+    private fun createPromptInfo(): BiometricPrompt.PromptInfo {
+
+        return BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric Authentication")
+            .setSubtitle("Please Take SS of this screen")
+            .setDescription(
+                "Please email me the ss. "
+            )
+            .setConfirmationRequired(true)
+            .setNegativeButtonText("Click here.")
             .build()
-
-        biometricPrompt.authenticate(promptInfo)
-
-
     }
 
     private fun checkBiometricStatus(bioMetricManager: BiometricManager) {
 
         when (bioMetricManager.canAuthenticate()) {
-            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> Log.d(
-                TAG,
-                "checkBiometricStatus: BIOMETRIC_ERROR_HW_UNAVAILABLE"
-            )
-            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> Log.d(
-                TAG,
-                "checkBiometricStatus: BIOMETRIC_ERROR_NONE_ENROLLED"
-            )
-            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> Log.d(
-                TAG,
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                type.text = "checkBiometricStatus: BIOMETRIC_ERROR_HW_UNAVAILABLE"
+
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                type.text =
+                    "checkBiometricStatus: BIOMETRIC_ERROR_NONE_ENROLLED"
+
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> type.text =
                 "checkBiometricStatus: BIOMETRIC_ERROR_NO_HARDWARE"
-            )
-            BiometricManager.BIOMETRIC_SUCCESS -> Log.d(
-                TAG,
+
+            BiometricManager.BIOMETRIC_SUCCESS -> type.text =
                 "checkBiometricStatus: BIOMETRIC_SUCCESS"
-            )
+
         }
 
     }
